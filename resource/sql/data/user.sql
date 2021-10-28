dcc65860027da05ad27d5cacb9543cdd3eaeb496
yota konno <yota.rock.guitar@gmail.com>
2021-10-28 23:15:38 +0900

    add relation user community

---
+++ resource/sql/data/user.sql
@@ -0,0 +1,20 @@
+-- user table
+SET CURRENT_SCHEMA=RCZ92613;
+
+INSERT INTO USER("USER_NAME",CREATED_AT,MODIFIED_AT) 
+    VALUES (
+        'テストユーザ1',
+        CURRENT_TIMESTAMP,
+        CURRENT_TIMESTAMP
+    );
+
+INSERT INTO USER("USER_NAME",CREATED_AT,MODIFIED_AT) 
+    VALUES (
+        'テストユーザ2',
+        CURRENT_TIMESTAMP,
+        CURRENT_TIMESTAMP
+    );
+
+
+
+
