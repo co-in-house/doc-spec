efad496e5d94adac96049ba7328baef3d8b0c635
yota konno <yota.rock.guitar@gmail.com>
2021-10-31 02:05:17 +0900

    fix event tbl def

--- resource/sql/data/event.sql
+++ resource/sql/data/event.sql
@@ -4,11 +4,11 @@ SET CURRENT_SCHEMA=RCZ92613;
 INSERT INTO EVENT(COMMUNITY_ID,TITLE,"START","END",LOCATION,DESCRIPTION,THUMBNAIL_IMG,CREATED_AT,MODIFIED_AT) 
     VALUES (
         1,
-        'テストイベント1',
-        '2019-10-01-12.00.00.000000',
-        '2019-10-01-19.00.00.000000',
-        '東京都千代田区紀尾井町７−１',
-        'コミュニティ1に紐づくエベント1',
+        'バンジージャンプに皆で挑戦しようぜ！',
+        '2021-10-30-12.00.00.000000',
+        '2021-10-30-19.00.00.000000',
+        '岐阜県八百津町久田味 新旅足端',
+        'お久しぶりです。田中です！ \n 改行ってはいるのかな？',
         'https://image.asoview-media.com/image/production/acp/3000016117/pln3000032120/a223704d-89ef-4326-931e-b382982edb25.jpeg',
         CURRENT_TIMESTAMP,
         CURRENT_TIMESTAMP
@@ -18,8 +18,8 @@ INSERT INTO EVENT(COMMUNITY_ID,TITLE,"START","END",LOCATION,DESCRIPTION,THUMBNAI
     VALUES (
         1,
         'テストイベント2',
-        '2019-10-01-12.00.00.000000',
-        '2019-10-01-19.00.00.000000',
+        '2021-10-30-13.00.00.000000',
+        '2021-10-30-18.00.00.000000',
         '東京都千代田区紀尾井町７−１',
         'コミュニティ1に紐づくエベント2',
         'https://image2.30min.jp/30min/tweet/1206/461206.jpg',
@@ -31,8 +31,8 @@ INSERT INTO EVENT(COMMUNITY_ID,TITLE,"START","END",LOCATION,DESCRIPTION,THUMBNAI
     VALUES (
         1,
         'テストイベント3',
-        '2019-10-01-12.00.00.000000',
-        '2019-10-01-19.00.00.000000',
+        '2021-10-30-15.00.00.000000',
+        '2021-10-30-18.00.00.000000',
         '東京都千代田区紀尾井町７−１',
         'コミュニティ1に紐づくエベント3',
         'https://www.tv-asahi.co.jp/animesong_sousenkyo/OG.jpg',
@@ -44,8 +44,8 @@ INSERT INTO EVENT(COMMUNITY_ID,TITLE,"START","END",LOCATION,DESCRIPTION,THUMBNAI
     VALUES (
         2,
         'テストイベント4',
-        '2019-10-01-12.00.00.000000',
-        '2019-10-01-19.00.00.000000',
+        '2021-10-31-15.00.00.000000',
+        '2021-10-31-18.00.00.000000',
         '東京都千代田区紀尾井町７−１',
         'コミュニティ2に紐づくエベント4',
         'https://image2.30min.jp/30min/tweet/1206/461206.jpg',
@@ -57,11 +57,50 @@ INSERT INTO EVENT(COMMUNITY_ID,TITLE,"START","END",LOCATION,DESCRIPTION,THUMBNAI
     VALUES (
         2,
         'テストイベント5',
-        '2019-10-01-12.00.00.000000',
-        '2019-10-01-19.00.00.000000',
+        '2021-10-31-16.00.00.000000',
+        '2021-10-31-18.00.00.000000',
         '東京都千代田区紀尾井町７−１',
         'コミュニティ2に紐づくエベント5',
         'https://image.asoview-media.com/image/production/acp/3000016117/pln3000032120/a223704d-89ef-4326-931e-b382982edb25.jpeg',
+        CURRENT_TIMESTAMP,
+        CURRENT_TIMESTAMP
+    );
+
+INSERT INTO EVENT(COMMUNITY_ID,TITLE,"START","END",LOCATION,DESCRIPTION,THUMBNAIL_IMG,CREATED_AT,MODIFIED_AT) 
+    VALUES (
+        3,
+        'testバンジージャンプに皆で挑戦しようぜ！',
+        '2021-11-01-12.00.00.000000',
+        '2021-11-01-19.00.00.000000',
+        '岐阜県八百津町久田味 新旅足端',
+        'お久しぶりです。田中です！ \n 改行ってはいるのかな？',
+        'https://image.asoview-media.com/image/production/acp/3000016117/pln3000032120/a223704d-89ef-4326-931e-b382982edb25.jpeg',
+        CURRENT_TIMESTAMP,
+        CURRENT_TIMESTAMP
+    );
+
+INSERT INTO EVENT(COMMUNITY_ID,TITLE,"START","END",LOCATION,DESCRIPTION,THUMBNAIL_IMG,CREATED_AT,MODIFIED_AT) 
+    VALUES (
+        2,
+        'テストイベント91',
+        '2021-11-02-13.00.00.000000',
+        '2021-11-02-18.00.00.000000',
+        '東京都千代田区紀尾井町７−１',
+        'コミュニティ1に紐づくエベント2',
+        'https://image2.30min.jp/30min/tweet/1206/461206.jpg',
+        CURRENT_TIMESTAMP,
+        CURRENT_TIMESTAMP
+    );
+
+INSERT INTO EVENT(COMMUNITY_ID,TITLE,"START","END",LOCATION,DESCRIPTION,THUMBNAIL_IMG,CREATED_AT,MODIFIED_AT) 
+    VALUES (
+        1,
+        'テストイベント92',
+        '2021-11-03-15.00.00.000000',
+        '2021-11-03-18.00.00.000000',
+        '東京都千代田区紀尾井町７−１',
+        'コミュニティ1に紐づくエベント3',
+        'https://www.tv-asahi.co.jp/animesong_sousenkyo/OG.jpg',
         CURRENT_TIMESTAMP,
         CURRENT_TIMESTAMP
     );
