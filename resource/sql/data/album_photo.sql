3eea097f52399d3dac8eea1c6bd24223c1d9db3b
yota konno <yota.rock.guitar@gmail.com>
2021-11-14 20:11:30 +0900

    fix album_photo def

---
+++ resource/sql/data/album_photo.sql
@@ -0,0 +1,96 @@
+-- album_photo data
+SET CURRENT_SCHEMA=RCZ92613;
+
+
+INSERT INTO ALBUM_PHOTO(ALBUM_ID,"URL",CREATED_AT,MODIFIED_AT) 
+    VALUES (
+        1,
+        'https://image2.30min.jp/30min/tweet/1206/461206.jpg',
+        CURRENT_TIMESTAMP,
+        CURRENT_TIMESTAMP
+    );
+INSERT INTO ALBUM_PHOTO(ALBUM_ID,"URL",CREATED_AT,MODIFIED_AT) 
+    VALUES (
+        1,
+        'https://image.asoview-media.com/image/production/acp/3000016117/pln3000032120/a223704d-89ef-4326-931e-b382982edb25.jpeg',
+        CURRENT_TIMESTAMP,
+        CURRENT_TIMESTAMP
+    );
+INSERT INTO ALBUM_PHOTO(ALBUM_ID,"URL",CREATED_AT,MODIFIED_AT) 
+    VALUES (
+        1,
+        'https://www.tv-asahi.co.jp/animesong_sousenkyo/OG.jpg',
+        CURRENT_TIMESTAMP,
+        CURRENT_TIMESTAMP
+    );
+
+INSERT INTO ALBUM_PHOTO(ALBUM_ID,"URL",CREATED_AT,MODIFIED_AT) 
+    VALUES (
+        1,
+        'https://image2.30min.jp/30min/tweet/1206/461206.jpg',
+        CURRENT_TIMESTAMP,
+        CURRENT_TIMESTAMP
+    );
+INSERT INTO ALBUM_PHOTO(ALBUM_ID,"URL",CREATED_AT,MODIFIED_AT) 
+    VALUES (
+        1,
+        'https://image.asoview-media.com/image/production/acp/3000016117/pln3000032120/a223704d-89ef-4326-931e-b382982edb25.jpeg',
+        CURRENT_TIMESTAMP,
+        CURRENT_TIMESTAMP
+    );
+INSERT INTO ALBUM_PHOTO(ALBUM_ID,"URL",CREATED_AT,MODIFIED_AT) 
+    VALUES (
+        1,
+        'https://www.tv-asahi.co.jp/animesong_sousenkyo/OG.jpg',
+        CURRENT_TIMESTAMP,
+        CURRENT_TIMESTAMP
+    );
+
+
+
+
+
+
+INSERT INTO ALBUM_PHOTO(ALBUM_ID,"URL",CREATED_AT,MODIFIED_AT) 
+    VALUES (
+        2,
+        'https://image2.30min.jp/30min/tweet/1206/461206.jpg',
+        CURRENT_TIMESTAMP,
+        CURRENT_TIMESTAMP
+    );
+INSERT INTO ALBUM_PHOTO(ALBUM_ID,"URL",CREATED_AT,MODIFIED_AT) 
+    VALUES (
+        2,
+        'https://image.asoview-media.com/image/production/acp/3000016117/pln3000032120/a223704d-89ef-4326-931e-b382982edb25.jpeg',
+        CURRENT_TIMESTAMP,
+        CURRENT_TIMESTAMP
+    );
+INSERT INTO ALBUM_PHOTO(ALBUM_ID,"URL",CREATED_AT,MODIFIED_AT) 
+    VALUES (
+        2,
+        'https://www.tv-asahi.co.jp/animesong_sousenkyo/OG.jpg',
+        CURRENT_TIMESTAMP,
+        CURRENT_TIMESTAMP
+    );
+
+INSERT INTO ALBUM_PHOTO(ALBUM_ID,"URL",CREATED_AT,MODIFIED_AT) 
+    VALUES (
+        3,
+        'https://image2.30min.jp/30min/tweet/1206/461206.jpg',
+        CURRENT_TIMESTAMP,
+        CURRENT_TIMESTAMP
+    );
+INSERT INTO ALBUM_PHOTO(ALBUM_ID,"URL",CREATED_AT,MODIFIED_AT) 
+    VALUES (
+        3,
+        'https://image.asoview-media.com/image/production/acp/3000016117/pln3000032120/a223704d-89ef-4326-931e-b382982edb25.jpeg',
+        CURRENT_TIMESTAMP,
+        CURRENT_TIMESTAMP
+    );
+INSERT INTO ALBUM_PHOTO(ALBUM_ID,"URL",CREATED_AT,MODIFIED_AT) 
+    VALUES (
+        4,
+        'https://www.tv-asahi.co.jp/animesong_sousenkyo/OG.jpg',
+        CURRENT_TIMESTAMP,
+        CURRENT_TIMESTAMP
+    );
